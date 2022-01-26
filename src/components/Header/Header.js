b3f32bbef9592f26e86fc9030ad718d54fb47a59
imreset <you@example.com>
2022-01-25 17:44:00 -0800

    remove unused dependacy

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { FaSpotify } from "react-icons/fa";
 import { SiDiscord } from "react-icons/si";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
