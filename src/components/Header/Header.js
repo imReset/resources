4edbffd6404a40fd6513af478ceba0f3f5918799
imreset <you@example.com>
2022-01-26 17:24:20 -0800

    change nav link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,7 +24,7 @@ const Header = () =>  (
       </li>
       <li>
         <Link href="#tech">
-          <NavLink>Technologies</NavLink>
+          <NavLink>Other Sites</NavLink>
         </Link>
       </li>        
       <li>
