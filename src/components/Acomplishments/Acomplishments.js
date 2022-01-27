3c93c9d00b00f44c5f135127cfbf98f8fa7c98e3
imReset <fffffreinds101@gmail.com>
2022-01-26 20:15:37 -0800

    update accomplisments

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -3,10 +3,11 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: "", text: 'Did a major revamp to this website'},
+  // Add more after each advancement
+  // { number: 1000, text: 'Students', },
+  // { number: 1900, text: 'Github Followers', },
+  // { number: 5000, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
